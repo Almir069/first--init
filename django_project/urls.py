13f24aae2633ec20d11cf7c9e05a238cba4b3921
unknown <muratovalmir456@gmail.com>
2024-10-20 11:00:56 -0700

    initial commit

---
+++ django_project/urls.py
@@ -0,0 +1,4 @@
+from django.contrib import admin # type: ignore
+from django.urls import path, include # type: ignore
+
+urlpatterns = [ path("admin/", admin.site.urls), path("", include("pages.urls")), ]
---
+++ pages/urls.py
@@ -0,0 +1,4 @@
+from django.urls import path # type: ignore
+from .views import homePageView
+
+urlpatterns = [ path("", homePageView, name="home"), ]
